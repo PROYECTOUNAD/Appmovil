6275743ee32ef826e9d9d67d2169ad536217619b
cpoveda77 <claudiamar06@yahoo.com>
2020-02-23 20:20:41 -0500

    test comentario

--- app/src/main/java/app/movil/pmcaso11/login.java
+++ app/src/main/java/app/movil/pmcaso11/login.java
@@ -7,16 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
     private Button registro;
+    private TextView recuperar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        recuperar=(TextView)findViewById(R.id.recuperar);
 
         registro = (Button) findViewById(R.id.registrar);
 
@@ -30,6 +34,19 @@ public class login extends AppCompatActivity {
 
                 }
             });
+
+            // Recuperar Pass
+
+            recuperar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent second = new Intent(login.this,activity_recover_password_new.class);
+                    startActivity(second);
+
+                }
+            });
+
 
     }
 
