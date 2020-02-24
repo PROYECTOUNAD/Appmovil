5ce6dd13e4cbcc4a3718ef0505a8eb4dc17510c8
cpoveda77 <claudiamar06@yahoo.com>
2020-02-23 20:20:41 -0500

    test comentario

--- app/src/main/java/app/movil/pmcaso11/login.java
+++ app/src/main/java/app/movil/pmcaso11/login.java
@@ -1,91 +1,79 @@
 package app.movil.pmcaso11;
 
-<<<<<<< HEAD
-=======
 import androidx.appcompat.app.AppCompatActivity;
 
->>>>>>> Creacion de Campos Formulario Registro
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-<<<<<<< HEAD
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-=======
->>>>>>> Creacion de Campos Formulario Registro
 
 public class login extends AppCompatActivity {
 
     private Button registro;
-<<<<<<< HEAD
     private Button ingreso;
     private EditText user;
     private EditText clave;
-    private TextView recovery;
-=======
 
->>>>>>> Creacion de Campos Formulario Registro
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-<<<<<<< HEAD
         registro =(Button)findViewById(R.id.registrar);
         ingreso=(Button)findViewById(R.id.ingresar);
         user=(EditText)findViewById(R.id.usuario);
         clave=(EditText)findViewById(R.id.pass);
-        recovery=(TextView)findViewById(R.id.recovery);
+
+
     }
 
         public void ingresar(View view){
 
-           /* AdminSqliteOpenHelper admin =new AdminSqliteOpenHelper(this,"DB15",null,1 );
+            AdminSqliteOpenHelper admin =new AdminSqliteOpenHelper(this,"DB15",null,1 );
             SQLiteDatabase db =admin.getReadableDatabase();
             String us=user.getText().toString();
             String cl=clave.getText().toString();
 
             Cursor registro =db.rawQuery("select emailUsuario, passUsuario from usuario where emailUsuario="+"'us'"+"and passUsuario="+"'cl'", null);
 
+            if(registro.moveToFirst()){
 
-*/
-                Intent second = new Intent(login.this,principal.class);
+
+                Intent second = new Intent(login.this,bienvenido.class);
                 startActivity(second);
 
                 Toast.makeText(this,"BIENVENIDO",Toast.LENGTH_LONG).show();
+                db.close();
+
+
+            }else{
+
+
+                Toast.makeText(this,"USUARIO O PASSWORD INCORRECTO",Toast.LENGTH_LONG).show();
+                db.close();
+
+            }
+
 
         }
+
 
         public void registrar(View view){
 
             Intent second = new Intent(login.this,registroUsu.class);
             startActivity(second);
 
+
+
         }
-        public void recovery(View view){
-
-        Intent recovery = new Intent ( login.this, RecoveryPassword.class );
-        startActivity(recovery);
-=======
-        registro = (Button) findViewById(R.id.registrar);
 
 
-            registro.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
 
-                        Intent second = new Intent(login.this,registroUsu.class);
-                        startActivity(second);
 
-                }
-            });
-
->>>>>>> Creacion de Campos Formulario Registro
-    }
 
 }
