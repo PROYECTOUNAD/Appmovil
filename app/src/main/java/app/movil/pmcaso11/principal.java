4d1eede21d7b455fce686b70e716d9b2e3b933dc
PROYECTOUNAD <59449292+PROYECTOUNAD@users.noreply.github.com>
2020-02-29 21:19:20 -0500

    Actualizacion Proyecto

--- app/src/main/java/app/movil/pmcaso11/principal.java
+++ app/src/main/java/app/movil/pmcaso11/principal.java
@@ -2,13 +2,41 @@ package app.movil.pmcaso11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class principal extends AppCompatActivity {
+
+    private Button zonas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        zonas=(Button)findViewById(R.id.zonasR);
+
+        zonas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent second = new Intent(principal.this,zonas.class);
+                startActivity(second);
+
+
+
+            }
+        });
+
+
     }
+
+
+
+
+
 }
